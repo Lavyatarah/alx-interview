da556f378b8c6b338f1198af2c84b7ed1c5439c9
Lavyatarah <mattlavyy@gmail.com>
2024-05-30 09:01:26 +0300

    updated

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,54 +1,66 @@
-#!/usr/bin/python3
-"""
-N queens
-"""
-
 import sys
 
-if len(sys.argv) != 2:
-    print('Usage: nqueens N')
-    exit(1)
-
-try:
-    n_q = int(sys.argv[1])
-except ValueError:
-    print('N must ba a number')
-    exit(1)
-
-if n_q < 4:
-    print('N must ba at least 4')
-    exit(1)
-
-
-def solve_nqueens(n):
-    ''' self descriptive '''
-    if n == 0:
-        return [[]]
-    inner_solution = solve_nqueens(n - 1)
-    return [solution + [(n, i + 1)]
-            for i in range(n_q)
-            for solution in inner_solution
-            if safe_queen((n, i + 1), solution)]
-
-
-def attack_queen(square, queen):
-    '''self descriptive'''
-    (row1, col1) = square
-    (row2, col2) = queen
-    return (row1 == row2) or (col1 == col2) or\
-        abs(row1 - row2) == abs(col1 - col2)
-
-
-def safe_queen(sqr, queens):
-    '''self descriptive'''
-    for queen in queens:
-        if attack_queen(sqr, queen):
+def is_safe(row, col, board):
+    """Checks if placing a queen at (row, col) is safe (no attacks)."""
+    n = len(board)
+    # Check for queens in the same row
+    for i in range(col):
+        if board[row][i] == 'Q':
             return False
+
+    # Check for queens in diagonals (upward and downward)
+    i, j = row, col
+    while i >= 0 and j >= 0:
+        if board[i][j] == 'Q':
+            return False
+        i -= 1
+        j -= 1
+
+    i, j = row, col
+    while i < n and j >= 0:
+        if board[i][j] == 'Q':
+            return False
+        i += 1
+        j -= 1
+
     return True
 
+def solve_nqueens(board, col, n):
+    """Solves the N-queens problem recursively."""
+    if col == n:
+        # Reached the last column, print the solution
+        for row in board:
+            print(''.join(row))
+        return
 
-for answer in reversed(solve_nqueens(n_q)):
-    result = []
-    for p in [list(p) for p in answer]:
-        result.append([i - 1 for i in p])
-    print(result)
+    for i in range(n):
+        # Check if placing a queen at (i, col) is safe
+        if is_safe(i, col, board):
+            board[i][col] = 'Q'
+            solve_nqueens(board, col + 1, n)  # Recursively solve for next column
+            board[i][col] = '.'  # Backtrack for other solutions
+
+def main():
+    """Handles program arguments and calls the N-queens solver."""
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N", file=sys.stderr)
+        sys.exit(1)
+
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number", file=sys.stderr)
+        sys.exit(1)
+
+    if n < 4:
+        print("N must be at least 4", file=sys.stderr)
+        sys.exit(1)
+
+    # Create an empty NxN board
+    board = [['.'] * n for _ in range(n)]
+
+    solve_nqueens(board, 0, n)
+
+if __name__ == "__main__":
+    main()
+
